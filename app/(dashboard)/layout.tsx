3daea0b9005dbe5549544d3636c585296024a873
Mahmoud Agbaria <mahmoud.moaad@gmail.com>
2023-09-27 21:33:43 +0300

    Remove env variables

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashboardLayout = ({ children }: { children: React.ReactInstance }) => {
+    return (
+        <div className='h-full relative'></div>
+    )
+}
+
+export default DashboardLayout
